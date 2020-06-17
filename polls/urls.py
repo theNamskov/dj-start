b663383cda9a687ad577126907fd3706364536e5
thenamskov <thenamskov@localhost.localdomain>
2020-06-16 20:21:23 -0500

    feat && update: template scripts && view updates

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('more-info', views.more_info, name="more-info"),
+	path('<int:question_id>/', views.detail, name='detail'),
+	path('<int:question_id>/results/', views.results, name='results'),
+	path('<int:question_id>/vote/', views.vote, name='vote'),
+	path('more-info', views.more_info, name='more-info'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,48 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('pub_date')[:5]
+	template_dir = 'polls/index.html'
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return render(request, template_dir, context)
 
 def more_info(request):
 	return HttpResponse("<h1>Over here we are starting with django and we gonna build a polls app. Stay tuned...</h1>")
+
+def detail(request, question_id):
+	template_dir = 'polls/detail.html'
+	question = get_object_or_404(Question, pk=question_id)
+	context = {
+		'question': question,
+	}
+	return render(request, template_dir, context)
+
+def results(request, question_id):
+	template_dir = 'polls/results.html'
+	question = get_object_or_404(Question, pk=question_id)
+	context = {
+		'question': question,
+	}
+	return render(request, template_dir, context)
+
+def vote(request, question_id):
+	template_dir = 'polls/detail.html'
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, template_dir, {
+			'error_message': "You didn't select a choice.", 'question': question,
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
